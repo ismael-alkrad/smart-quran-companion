a98eb8d6e33e9f6ba6a067694b0afaed3e0bcd89
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 22:29:37 +0300

    fix: keep Mushaf page header fully RTL

--- src/modules/quran/components/MushafPageHeader.vue
+++ src/modules/quran/components/MushafPageHeader.vue
@@ -17,7 +17,8 @@ const surahTitle = computed(() => {
 
 <template>
   <header
-    class="relative grid min-h-[45px] grid-cols-[minmax(0,1fr)_auto] items-center gap-[18px] rounded-[18px] border border-[#b8a27f]/90 px-5 py-[7px] text-[0.9rem] text-[#2d2822] [direction:ltr] [font-family:'Noto_Naskh_Arabic','Amiri',serif] before:absolute before:top-1/2 before:-right-[5px] before:h-2 before:w-2 before:-translate-y-1/2 before:rotate-45 before:border before:border-[#b8a27f] before:bg-[#fbf7ef] before:content-[''] after:absolute after:top-1/2 after:-left-[5px] after:h-2 after:w-2 after:-translate-y-1/2 after:rotate-45 after:border after:border-[#b8a27f] after:bg-[#fbf7ef] after:content-['']"
+    dir="rtl"
+    class="relative grid min-h-[45px] grid-cols-[minmax(0,1fr)_auto] items-center gap-[18px] rounded-[18px] border border-[#b8a27f]/90 px-5 py-[7px] text-[0.9rem] text-[#2d2822] [font-family:'Noto_Naskh_Arabic','Amiri',serif] before:absolute before:top-1/2 before:-right-[5px] before:h-2 before:w-2 before:-translate-y-1/2 before:rotate-45 before:border before:border-[#b8a27f] before:bg-[#fbf7ef] before:content-[''] after:absolute after:top-1/2 after:-left-[5px] after:h-2 after:w-2 after:-translate-y-1/2 after:rotate-45 after:border after:border-[#b8a27f] after:bg-[#fbf7ef] after:content-['']"
     aria-label="معلومات الصفحة"
   >
     <span class="justify-self-start overflow-hidden text-ellipsis whitespace-nowrap font-semibold" dir="rtl">
