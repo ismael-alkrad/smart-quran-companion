ff05fe24c20ffeea02fc9384572ee70d124ff223
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 14:51:26 +0300

    feat(api): export shared Frappe API helpers

---
+++ src/shared/api/index.ts
@@ -0,0 +1,7 @@
+export {
+  FrappeResponseError,
+  SMART_QURAN_API_PREFIX,
+  smartQuranApiUrl,
+  useSmartQuranCall,
+} from '@/shared/api/frappe'
+export type { SmartQuranCallOptions } from '@/shared/api/frappe'
