c9ed92cea8d43bedb379c976a12970016fb7488b
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 21:47:07 +0300

    feat: export Home components

---
+++ src/modules/home/components/index.ts
@@ -0,0 +1,3 @@
+export { default as HomeTodayTaskRow } from '@/modules/home/components/HomeTodayTaskRow.vue'
+export { default as HomeTasmeeCta } from '@/modules/home/components/HomeTasmeeCta.vue'
+export { default as HomeLastQuranLocation } from '@/modules/home/components/HomeLastQuranLocation.vue'
