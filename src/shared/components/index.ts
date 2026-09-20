13bfd1c887a283b9fb644f82b054479e467e9bd7
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 15:13:20 +0300

    feat(ui): export base icon button

--- src/shared/components/index.ts
+++ src/shared/components/index.ts
@@ -1,2 +1,3 @@
 export { default as BaseButton } from '@/shared/components/BaseButton.vue'
 export { default as BaseInput } from '@/shared/components/BaseInput.vue'
+export { default as BaseIconButton } from '@/shared/components/BaseIconButton.vue'\n
