a768238f1e00b76d84a9affc80e24a5486f242b9
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 22:55:56 +0300

    feat: add Completed Day Home preview

---
+++ src/modules/home/pages/HomeCompletedDayPage.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { HomeDayOverview } from '@/modules/home'
+</script>
+
+<template>
+  <HomeDayOverview
+    subtitle="أنجزت خطتك القرآنية لهذا اليوم"
+    wird-state="done"
+    hifz-state="done"
+    murajaah-state="done"
+    progress-section-title="إغلاق اليوم"
+    progress-title="خطة اليوم مكتملة"
+    progress-meta="تم إنجاز الورد والحفظ والمراجعة"
+    reading-section-title="آخر موضع"
+    :show-attention="false"
+  />
+</template>
