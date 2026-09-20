6de66ce3373cabe10d6d1a05541ee460bb362db2
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 22:26:47 +0300

    fix: make bottom navigation natively RTL

--- src/shared/components/BaseBottomNav.vue
+++ src/shared/components/BaseBottomNav.vue
@@ -60,19 +60,19 @@ function routeFor(value: BaseBottomNavValue) {
 
 <template>
   <FrappeMobileNav
-    dir="ltr"
+    dir="rtl"
     class="!flex !h-[76px] !w-full !shrink-0 !items-center !justify-between !border-t-0 !bg-[var(--sqc-color-background-elevated)] !px-[var(--sqc-dimension-spacing-12)] !pb-0 [font-family:var(--sqc-font-family-ui)] [@media(display-mode:standalone)]:!pb-0 [&_[data-slot=mobile-nav-item]]:!h-[56px] [&_[data-slot=mobile-nav-item]]:!min-h-0 [&_[data-slot=mobile-nav-item]]:!w-[62px] [&_[data-slot=mobile-nav-item]]:!shrink-0 [&_[data-slot=mobile-nav-item]]:!gap-[4px] [&_[data-slot=mobile-nav-item]]:!py-0 [&_[data-slot=mobile-nav-item]:nth-child(3)]:!h-[66px] [&_[data-slot=mobile-nav-item]>span:last-child]:!text-[12px] [&_[data-slot=mobile-nav-item]>span:last-child]:!font-medium [&_[data-slot=mobile-nav-item]>span:last-child]:!leading-[18px] [&_[data-slot=mobile-nav-item][data-state=active]>span:last-child]:!text-[color:var(--sqc-color-text-brand)] [&_[data-slot=mobile-nav-item][data-state=inactive]>span:last-child]:!text-[color:var(--sqc-color-text-tertiary)]"
   >
     <FrappeMobileNavItem
-      :label="labels.profile"
-      :to="routeFor('profile')"
-      :active="modelValue === 'profile'"
-      @click="select('profile')"
+      :label="labels.home"
+      :to="routeFor('home')"
+      :active="modelValue === 'home'"
+      @click="select('home')"
     >
       <template #default="{ active }">
         <span
           aria-hidden="true"
-          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-profile-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-profile-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
+          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-home-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-home-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
           :class="
             active
               ? 'bg-[var(--sqc-color-text-brand)]'
@@ -83,15 +83,15 @@ function routeFor(value: BaseBottomNavValue) {
     </FrappeMobileNavItem>
 
     <FrappeMobileNavItem
-      :label="labels.groups"
-      :to="routeFor('groups')"
-      :active="modelValue === 'groups'"
-      @click="select('groups')"
+      :label="labels.quran"
+      :to="routeFor('quran')"
+      :active="modelValue === 'quran'"
+      @click="select('quran')"
     >
       <template #default="{ active }">
         <span
           aria-hidden="true"
-          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-groups-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-groups-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
+          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-quran-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-quran-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
           :class="
             active
               ? 'bg-[var(--sqc-color-text-brand)]'
@@ -120,15 +120,15 @@ function routeFor(value: BaseBottomNavValue) {
     </FrappeMobileNavItem>
 
     <FrappeMobileNavItem
-      :label="labels.quran"
-      :to="routeFor('quran')"
-      :active="modelValue === 'quran'"
-      @click="select('quran')"
+      :label="labels.groups"
+      :to="routeFor('groups')"
+      :active="modelValue === 'groups'"
+      @click="select('groups')"
     >
       <template #default="{ active }">
         <span
           aria-hidden="true"
-          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-quran-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-quran-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
+          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-groups-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-groups-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
           :class="
             active
               ? 'bg-[var(--sqc-color-text-brand)]'
@@ -139,15 +139,15 @@ function routeFor(value: BaseBottomNavValue) {
     </FrappeMobileNavItem>
 
     <FrappeMobileNavItem
-      :label="labels.home"
-      :to="routeFor('home')"
-      :active="modelValue === 'home'"
-      @click="select('home')"
+      :label="labels.profile"
+      :to="routeFor('profile')"
+      :active="modelValue === 'profile'"
+      @click="select('profile')"
     >
       <template #default="{ active }">
         <span
           aria-hidden="true"
-          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-home-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-home-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
+          class="block size-[20px] shrink-0 [mask-image:url('/assets/icons/nav-profile-20.svg')] [mask-position:center] [mask-repeat:no-repeat] [mask-size:100%_100%] [-webkit-mask-image:url('/assets/icons/nav-profile-20.svg')] [-webkit-mask-position:center] [-webkit-mask-repeat:no-repeat] [-webkit-mask-size:100%_100%]"
           :class="
             active
               ? 'bg-[var(--sqc-color-text-brand)]'
