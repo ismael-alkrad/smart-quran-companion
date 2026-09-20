0c87a1b4f33acaea0a6058cdb09bc4601c190814
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 17:37:19 +0300

    refactor(auth): export login session composables

--- src/modules/auth/composables/index.ts
+++ src/modules/auth/composables/index.ts
@@ -1,1 +1,3 @@
 export { useEmailVerificationFlow } from '@/modules/auth/composables/useEmailVerificationFlow'
+export { useAuthSession } from '@/modules/auth/composables/useAuthSession'
+export { useLoginFlow } from '@/modules/auth/composables/useLoginFlow'
