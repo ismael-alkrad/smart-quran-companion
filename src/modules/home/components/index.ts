4bc7339acb2041b1e105c092f519d9f33cd2e901
Ismael Alkrad <Esmaelalkrad0@gmail.com>
2026-09-20 22:08:48 +0300

    feat: export responsive Home scaffold

--- src/modules/home/components/index.ts
+++ src/modules/home/components/index.ts
@@ -1,3 +1,4 @@
+export { default as HomePageScaffold } from '@/modules/home/components/HomePageScaffold.vue'
 export { default as HomeTodayTaskRow } from '@/modules/home/components/HomeTodayTaskRow.vue'
 export { default as HomeTasmeeCta } from '@/modules/home/components/HomeTasmeeCta.vue'
 export { default as HomeLastQuranLocation } from '@/modules/home/components/HomeLastQuranLocation.vue'
